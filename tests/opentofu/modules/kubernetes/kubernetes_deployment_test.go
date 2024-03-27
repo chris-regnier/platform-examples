ca941880e44a5bda0b543a66c3f9183c1eb36f31
Chris Regnier <christopher.regnier@gmail.com>
2024-03-26 20:06:50 -0600

    Clean up, add output tests and better organization, remove hello world

--- tests/opentofu/modules/kubernetes/kubernetes_deployment_test.go
+++ tests/opentofu/modules/kubernetes/kubernetes_deployment_test.go
@@ -2,16 +2,13 @@ package test
 
 import (
 	"fmt"
-	"os/exec"
 	"path/filepath"
+	"strings"
 	"testing"
 	"text/template"
 
 	"os"
 
-	"sigs.k8s.io/kind/pkg/cluster"
-
-	"github.com/gruntwork-io/terratest/modules/docker"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/stretchr/testify/assert"
@@ -22,9 +19,6 @@ type ProviderConfig struct {
 	Context    string
 }
 
-const kindClusterName string = "kubernetes-deployment-test"
-const kindConfigPath string = "../../resources/kube-config.yaml"
-const kindContext string = "kind-" + kindClusterName
 const providerTemplate = `
 provider "kubernetes" {
 	config_path = "{{.ConfigPath}}"
@@ -34,69 +28,6 @@ provider "kubernetes" {
 const rootDirectory string = "../../../../"
 const rootOpenTofuDirectory string = "../../../../deployments/opentofu/modules"
 
-func TestMain(m *testing.M) {
-	err := createKindCluster()
-	if err != nil {
-		println("Failed to create kind cluster")
-		fmt.Printf("Error: %v", err)
-		os.Exit(2)
-	}
-
-	exitVal := m.Run()
-
-	err = deleteKindCluster()
-	if err != nil {
-		println("Failed to delete kind cluster")
-		fmt.Printf("Error: %v", err)
-		os.Exit(3)
-	}
-
-	os.Remove(kindConfigPath)
-
-	os.Exit(exitVal)
-}
-
-func buildAndLoadDockerImage(t *testing.T, contextPath string, dockerfilePath string, tag string) {
-	buildOptions := &docker.BuildOptions{
-		Tags:         []string{tag, fmt.Sprintf("docker.io/library/%s", tag), fmt.Sprintf("localhost/%s", tag)},
-		OtherOptions: []string{"-f", rootDirectory + dockerfilePath},
-	}
-	docker.Build(t, contextPath, buildOptions)
-}
-
-func createKindCluster() error {
-	provider := cluster.NewProvider()
-	err := provider.Create(
-		kindClusterName,
-		cluster.CreateWithNodeImage("kindest/node:v1.21.1"),
-		cluster.CreateWithKubeconfigPath(kindConfigPath),
-	)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func deleteKindCluster() error {
-	provider := cluster.NewProvider()
-	err := provider.Delete(kindClusterName, kindConfigPath)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func loadPodmanImageToKindCluster(image string) error {
-	command := exec.Command("kind", "load", "docker-image", "-n", kindClusterName, image)
-	err := command.Run()
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-		return err
-	}
-
-	return nil
-}
-
 func createKubernetesProviderFile(t *testing.T, config ProviderConfig, tempDirectory string) {
 	// Validate template
 	template, err := template.New("provider").Parse(providerTemplate)
@@ -104,14 +35,12 @@ func createKubernetesProviderFile(t *testing.T, config ProviderConfig, tempDirec
 		t.Fatalf("Failed to create template: %v", err)
 	}
 
-	// Create the destination file
-	destFile, err := os.Create(tempDirectory + "/__testing_provider.tf")
+	destFile, err := os.Create(tempDirectory + "/__test_provider.tf")
 	if err != nil {
 		t.Fatalf("Failed to create destination file: %v", err)
 	}
 	defer destFile.Close()
 
-	// Write the template to the destination file
 	err = template.Execute(destFile, config)
 	if err != nil {
 		t.Fatalf("Failed to write template to file: %v", err)
@@ -125,12 +54,14 @@ func createKubernetesProviderFile(t *testing.T, config ProviderConfig, tempDirec
 }
 
 func TestTerraformHelloWorldExample(t *testing.T) {
+	t.Parallel()
+	applicationNamespace := strings.ToLower(t.Name())
 	applicationName := "hello-world"
 	applicationImageRepository := fmt.Sprintf("localhost/%s", applicationName)
 	applicationImageTag := "local-test"
 	applicationFullImageName := fmt.Sprintf("%s:%s", applicationImageRepository, applicationImageTag)
-	// Build and load the docker image
-	buildAndLoadDockerImage(t, rootDirectory, "containers/python/Dockerfile", applicationFullImageName)
+
+	buildContainerImage(t, rootDirectory, "containers/python/Dockerfile", applicationFullImageName)
 
 	err := loadPodmanImageToKindCluster(applicationFullImageName)
 	assert.NoError(t, err)
@@ -144,21 +75,28 @@ func TestTerraformHelloWorldExample(t *testing.T) {
 	}
 	createKubernetesProviderFile(t, provider, tempDirectory)
 
-	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: tempDirectory,
 		NoColor:      true,
 		Vars: map[string]interface{}{
-			"use_local_volume":             false,
 			"application_image_repository": applicationImageRepository,
 			"application_image_tag":        applicationImageTag,
+			"application_name":             applicationName,
+			"namespace":                    applicationNamespace,
+			"use_local_volume":             false,
 		},
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
 
-	terraform.InitAndApply(t, terraformOptions)
+	terraform.InitAndApplyAndIdempotent(t, terraformOptions)
 
-	output := terraform.Output(t, terraformOptions, "hello_world")
-	assert.Equal(t, "Hello, World!", output)
+	actualNamepaceName := terraform.Output(t, terraformOptions, "namespace_name")
+	assert.Equal(t, applicationNamespace, actualNamepaceName)
+
+	actualDeploymentName := terraform.Output(t, terraformOptions, "deployment_name")
+	assert.Equal(t, applicationName, actualDeploymentName)
+
+	actualServiceName := terraform.Output(t, terraformOptions, "service_name")
+	assert.Equal(t, applicationName, actualServiceName)
 }
